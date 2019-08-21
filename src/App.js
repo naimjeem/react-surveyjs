d1d217b2a91e4f8fbd219d0c9993c5ff6d087756
Naim Jeem <naim36208@gmail.com>
2019-08-22 03:23:31 +0600

    add surveys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
-import SurveyCreator from "./SurveyCreator";
+// import SurveyCreator from "./SurveyCreator";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -41,218 +41,194 @@ widgets.bootstrapslider(Survey);
 
 class App extends Component {
   json = {
-    title: "Product Feedback Survey Example",
-    showProgressBar: "top",
-    pages: [
-      {
-        elements: [
-          {
-            "type": "radiogroup",
-            "name": "position",
-            "title": "Choose job position...",
-            "isRequired": true,
-            "colCount": 0,
-            "choices": ["1|Designer", "2|Front-end Developer", "3|Back-end Developer", "4|Database Administrator", "5|System Engineer"]
-          },
-          {
-            type: "barrating",
-            name: "barrating1",
-            ratingTheme: "css-stars",
-            title: "Please rate the movie you've just watched",
-            choices: ["1", "2", "3", "4", "5"]
-          },
-          {
-            type: "imagepicker",
-            name: "choosepicture",
-            title: "What animal would you like to see first ?",
-            choices: [
-              {
-                value: "lion",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
-              },
-              {
-                value: "giraffe",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
-              },
-              {
-                value: "panda",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
-              },
-              {
-                value: "camel",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
-              }
-            ]
-          },
-          {
-            type: "bootstrapslider",
-            name: "bootstrapslider"
-          },
-          {
-            type: "dropdown",
-            renderAs: "select2",
-            choicesByUrl: {
-              url: "https://restcountries.eu/rest/v1/all"
-            },
-            name: "countries",
-            title: "Please select the country you have arrived from:"
-          },
-          {
-            type: "signaturepad",
-            name: "sign",
-            title: "Please enter your signature"
-          },
-          {
-            type: "sortablelist",
-            name: "lifepriopity",
-            title: "Life Priorities ",
-            isRequired: true,
-            colCount: 0,
-            choices: ["family", "work", "pets", "travels", "games"]
-          },
-          {
-            name: "date",
-            type: "datepicker",
-            inputType: "date",
-            title: "Your favorite date:",
-            dateFormat: "mm/dd/yy",
-            isRequired: true
-          }
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "radiogroup",
+        "name": "question1",
+        "title": "Sex",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Male"
+         },
+         {
+          "value": "item2",
+          "text": "Female"
+         }
         ]
-      },
-      {
-        questions: [
-          {
-            type: "signaturepad",
-            width: "500px",
-            name: "Signature Pad - you can enter your signature here:"
-          },
-          {
-            type: "matrix",
-            name: "Quality",
-            title:
-              "Please indicate if you agree or disagree with the following statements",
-            columns: [
-              {
-                value: 1,
-                text: "Strongly Disagree"
-              },
-              {
-                value: 2,
-                text: "Disagree"
-              },
-              {
-                value: 3,
-                text: "Neutral"
-              },
-              {
-                value: 4,
-                text: "Agree"
-              },
-              {
-                value: 5,
-                text: "Strongly Agree"
-              }
-            ],
-            rows: [
-              {
-                value: "affordable",
-                text: "Product is affordable"
-              },
-              {
-                value: "does what it claims",
-                text: "Product does what it claims"
-              },
-              {
-                value: "better then others",
-                text: "Product is better than other products on the market"
-              },
-              {
-                value: "easy to use",
-                text: "Product is easy to use"
-              }
-            ]
-          },
-          {
-            type: "rating",
-            name: "satisfaction",
-            title: "How satisfied are you with the Product?",
-            mininumRateDescription: "Not Satisfied",
-            maximumRateDescription: "Completely satisfied"
-          },
-          {
-            type: "rating",
-            name: "recommend friends",
-            visibleIf: "{satisfaction} > 3",
-            title:
-              "How likely are you to recommend the Product to a friend or co-worker?",
-            mininumRateDescription: "Will not recommend",
-            maximumRateDescription: "I will recommend"
-          },
-          {
-            type: "comment",
-            name: "suggestions",
-            title: "What would make you more satisfied with the Product?"
-          }
+       },
+       {
+        "type": "radiogroup",
+        "name": "question2",
+        "title": "Do you drink alcohol?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
         ]
-      },
-      {
-        questions: [
-          {
-            type: "radiogroup",
-            name: "price to competitors",
-            title: "Compared to our competitors, do you feel the Product is",
-            choices: [
-              "Less expensive",
-              "Priced about the same",
-              "More expensive",
-              "Not sure"
-            ]
-          },
-          {
-            type: "radiogroup",
-            name: "price",
-            title: "Do you feel our current price is merited by our product?",
-            choices: [
-              "correct|Yes, the price is about right",
-              "low|No, the price is too low for your product",
-              "high|No, the price is too high for your product"
-            ]
-          },
-          {
-            type: "multipletext",
-            name: "pricelimit",
-            title: "What is the... ",
-            items: [
-              {
-                name: "mostamount",
-                title: "Most amount you would every pay for a product like ours"
-              },
-              {
-                name: "leastamount",
-                title: "The least amount you would feel comfortable paying"
-              }
-            ]
-          }
+       },
+       {
+        "type": "radiogroup",
+        "name": "question3",
+        "title": "Do you have / had an alcohol problem?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
         ]
-      },
-      {
-        questions: [
-          {
-            type: "text",
-            name: "email",
-            title:
-              'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
-          }
+       },
+       {
+        "type": "radiogroup",
+        "name": "question4",
+        "title": "Do other people think you have / had an alcohol problem?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
         ]
-      }
+       },
+       {
+        "type": "radiogroup",
+        "name": "question5",
+        "title": "Have you ever used illicit or street drugs?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question6",
+        "title": "Do you have / had an drug problem?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question7",
+        "title": "Do other people think you have / had a drug problem?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question8",
+        "title": "Do you have / had Cirrhosis?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question9",
+        "title": "Do you have / had Other Liver problem?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "paneldynamic",
+        "name": "question10",
+        "templateElements": [
+         {
+          "type": "radiogroup",
+          "name": "question12",
+          "title": "Do you have / had Other Liver problem?",
+          "choices": [
+           {
+            "value": "item1",
+            "text": "Yes"
+           },
+           {
+            "value": "item2",
+            "text": "No"
+           }
+          ]
+         },
+         {
+          "type": "radiogroup",
+          "name": "question13",
+          "title": "When finished?",
+          "choices": [
+           {
+            "value": "item1",
+            "text": "Early"
+           },
+           {
+            "value": "item2",
+            "text": "Mid"
+           },
+           {
+            "value": "item3",
+            "text": "Late"
+           }
+          ]
+         },
+         {
+          "type": "text",
+          "name": "question14",
+          "title": "Comment"
+         }
+        ]
+       }
+      ]
+     }
     ]
-  };
-
+   }
   onValueChanged(result) {
     console.log("value changed!");
   }
@@ -279,12 +255,12 @@ class App extends Component {
             onValueChanged={this.onValueChanged}
           />
           {/*If you do not want to show Survey Creator, comment the line below*/}
-          <h1>SurveyJS Creator in action:</h1>
-          <SurveyCreator />
+          {/* <h1>SurveyJS Creator in action:</h1>
+          <SurveyCreator /> */}
         </div>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
